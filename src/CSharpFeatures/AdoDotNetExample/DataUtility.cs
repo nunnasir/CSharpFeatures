073ddaa48bf79ac169ebc06c8f500fb1ba5ca430
Nasir Uddin <nunnasir@gmail.com>
2022-04-11 04:23:23 +0600

    Ado dot net example

---
+++ src/CSharpFeatures/AdoDotNetExample/DataUtility.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace AdoDotNetExample
+{
+    class DataUtility
+    {
+        private readonly string _connectionString;
+        public DataUtility(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        // Sql Insert, Update, Delete Operations
+        // Without Parameter 
+        public void ExecuteCommand(string query)
+        {
+            using SqlConnection connection = new(_connectionString);
+            using SqlCommand command = connection.CreateCommand();
+
+            command.CommandText = query;
+
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+
+            command.ExecuteNonQuery();
+        }
+
+        // Sql Insert, Update, Delete Operations
+        // With Parameter
+        public void ExecuteCommand(string query, List<(string key, object value)> parameters)
+        {
+            using SqlConnection connection = new(_connectionString);
+            using SqlCommand command = connection.CreateCommand();
+
+            command.CommandText = query;
+
+            if (parameters != null && parameters.Count > 0)
+            {
+                foreach (var (key, value) in parameters)
+                {
+                    command.Parameters.Add(new SqlParameter(key, value));
+                }
+            }
+
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+
+            command.ExecuteNonQuery();
+        }
+
+        // Sql Select Operations
+        // Without Parameter
+        public IList<Dictionary<string, object>> GetDatas(string query)
+        {
+            using SqlConnection connection = new(_connectionString);
+            using SqlCommand command = connection.CreateCommand();
+
+            command.CommandText = query;
+
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Dictionary<string, object>> columns = new();
+
+            while (reader.Read())
+            {
+                //int id = (int)reader[0];
+                //int id = (int)reader["name"];
+                //var columns = reader.GetColumnSchema();
+                //string column1 = columns[0].ColumnName;
+
+                Dictionary<string, object> rows = new();
+                foreach (var column in reader.GetColumnSchema())
+                {
+                    rows.Add(column.ColumnName, reader[column.ColumnName]);
+                }
+                columns.Add(rows);
+            }
+
+            return columns;
+        }
+
+        // Sql Select Operations
+        // With Parameter
+        public IList<Dictionary<string, object>> GetDatas(string query, List<(string, object)> parameters)
+        {
+            using SqlConnection connection = new(_connectionString);
+            using SqlCommand command = connection.CreateCommand();
+
+            command.CommandText = query;
+
+            if (parameters != null && parameters.Count > 0)
+            {
+                foreach (var (key, value) in parameters)
+                {
+                    command.Parameters.Add(new SqlParameter(key, value));
+                }
+            }
+
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Dictionary<string, object>> columns = new();
+
+            while (reader.Read())
+            {
+                Dictionary<string, object> rows = new();
+                foreach (var column in reader.GetColumnSchema())
+                {
+                    rows.Add(column.ColumnName, reader[column.ColumnName]);
+                }
+                columns.Add(rows);
+            }
+
+            return columns;
+        }
+    }
+}
---
+++ src/CSharpFeatures/AdoDotNetExample/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdoDotNetExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Database and Table Schema
+            string connectionString = "Server=DESKTOP-H28I08B;Database=csharpb9;User Id=csharpb9;Password=123456";
+
+            //DeleteData(connectionString);
+            //InsertData(connectionString);
+            GetData(connectionString);
+            //UpdateData(connectionString);
+        }
+
+        public static void InsertData(string connectionString)
+        {
+            DataUtility utility = new(connectionString);
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Birthday: ");
+            string birthDate = Console.ReadLine();
+
+            string query = "INSERT INTO STUDENT([Name], DateOfBirth) VALUES(@name, @dateOfBirth)";
+
+            List<(string, object)> parameters = new()
+            {
+                ("name", name),
+                ("dateOfBirth", birthDate),
+            };
+
+            utility.ExecuteCommand(query, parameters);
+        }
+
+        public static void UpdateData(string connectionString)
+        {
+            DataUtility dataUtility = new DataUtility(connectionString);
+
+            Console.Write("Your Updated Name: ");
+            string updateName = Console.ReadLine();
+
+            Console.Write("Your Updated Value Id: ");
+            string id = Console.ReadLine();
+
+            string updateQuery = "UPDATE STUDENT SET [Name] = @name WHERE id = @id";
+
+            List<(string, object)> parameters = new()
+            {
+                ("name", updateName),
+                ("id", id)
+            };
+
+            dataUtility.ExecuteCommand(updateQuery, parameters);
+        }
+
+        public static void DeleteData(string connectionString)
+        {
+            DataUtility dataUtility = new DataUtility(connectionString);
+
+            string deleteQuery = "DELETE FROM STUDENT";
+
+            dataUtility.ExecuteCommand(deleteQuery);
+        }
+
+        public static void GetData(string connectionString)
+        {
+            DataUtility dataUtility = new DataUtility(connectionString);
+            string selectQuery = "SELECT * FROM STUDENT";
+
+            var result = dataUtility.GetDatas(selectQuery, null);
+            //var result = dataUtility.GetDatas(selectQuery);
+
+            foreach (var item in result[0])
+            {
+                Console.Write(item.Key + "\t");
+            }
+            Console.WriteLine();
+            foreach (var rows in result)
+            {
+                foreach (var column in rows.Values)
+                {
+                    Console.Write(column + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
